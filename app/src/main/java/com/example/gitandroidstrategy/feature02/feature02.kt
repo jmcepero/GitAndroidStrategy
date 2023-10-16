696199f500f1f4b722026b2dd85004ba7c2b4bee
j.cepero <jose.cepero@reign.cl>
2023-10-16 11:51:05 -0300

    Revert "Feature 04 agregada (#4)"
    This reverts commit 412cbf91f804da563f1cac783963281322b011bf.

--- app/src/main/java/com/example/gitandroidstrategy/feature02/feature02.kt
+++ app/src/main/java/com/example/gitandroidstrategy/feature02/feature02.kt
@@ -8,7 +8,3 @@ package com.example.gitandroidstrategy.feature02
 // Feature 03 agregada en submodulo Feature 02
 // Commit 02 de Feature 03
 // Commit 03 de Feature 03
-
-// Feature 04 agregada en submodulo Feature 02
-// Commit 02 de Feature 04
-// Commit 03 de Feature 04
